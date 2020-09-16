abaa39ca4c5137e7e5358bc0409646cc00350a2f
I-Am-Lain <vetter.william@gmail.com>
2020-09-16 09:45:10 -0700

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -28,4 +28,48 @@ class Person
         end
     end
 
+    def clean?
+        @hygiene > 7
+    end
+
+    def happy?
+        @happiness > 7
+    end
+
+    def get_paid(salary_amount)
+        @bank_account += salary_amount
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(person_class=Person.new("friend"))
+        self.happiness += 3
+        person_class.happiness += 3
+        return "Hi #{person_class.name}! It's #{@name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            return "blah blah sun blah rain"
+        else
+            return "blah blah blah blah blah"
+        end
+    end
+
 end
