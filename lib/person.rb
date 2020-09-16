25ab3e83e42e17ef703923ab8e83824f12d7b642
tincogs <tomoday91@gmail.com>
2020-09-16 11:11:17 -0500

    Initial

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,31 @@
-# your code goes here
+class Person
+    attr_reader :name, :hygiene, :happiness
+    attr_accessor :bank_account
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(new_happiness)
+        if new_happiness < 0
+            @happiness = 0
+        elsif new_happiness > 10
+            @happiness = 10
+        else
+            @happiness = new_happiness
+        end
+    end
+
+    def hygiene=(new_hygiene)
+        if new_hygiene < 0
+            @hygiene = 0
+        elsif new_hygiene > 10
+            @hygiene = 10
+        else
+            @hygiene = new_hygiene
+        end
+    end
+
+end
